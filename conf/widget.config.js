8e1e55edd0754201e1ac703c30009562f08f15eb
ywyz <admin@ywyz.tech>
2025-10-04 11:19:04 +0800

    更新配置文件，修改主题和小部件设置，调整默认值以优化功能

--- conf/widget.config.js
+++ conf/widget.config.js
@@ -1,3 +1,11 @@
+/*
+ * @Date: 2025-10-03 17:01:15
+ * @Author: ywyz
+ * @LastModifiedBy: ywyz
+ * @Github: https://github.com/ywyz
+ * @LastEditors: ywyz
+ * @LastEditTime: 2025-10-04 11:13:35
+ */
 /**
  * 悬浮在网页上的挂件
  */
@@ -15,7 +23,7 @@ module.exports = {
   DIFY_CHATBOT_TOKEN: process.env.NEXT_PUBLIC_DIFY_CHATBOT_TOKEN || '',
 
   // 悬浮挂件
-  WIDGET_PET: process.env.NEXT_PUBLIC_WIDGET_PET || true, // 是否显示宠物挂件
+  WIDGET_PET: process.env.NEXT_PUBLIC_WIDGET_PET || false, // 是否显示宠物挂件
   WIDGET_PET_LINK:
     process.env.NEXT_PUBLIC_WIDGET_PET_LINK ||
     'https://cdn.jsdelivr.net/npm/live2d-widget-model-wanko@1.0.5/assets/wanko.model.json', // 挂件模型地址 @see https://github.com/xiazeyu/live2d-widget-models
